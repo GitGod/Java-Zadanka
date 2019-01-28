70fd73053da7f5f9ae043197e54ef209610f6c97
GitGod <barmazu23@gmail.com>
2019-01-28 13:20:48 +0100

    Create class Node

---
+++ Algorytmy/Kolejka/Node.java
@@ -0,0 +1,10 @@
+public class Node<E> {
+    private E element;
+    Node next;
+    public Node(E element) {
+        this.element = element;
+    }
+    public E getElement() {
+        return element;
+    }
+}
